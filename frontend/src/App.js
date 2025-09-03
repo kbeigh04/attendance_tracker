ece3ff4f7300c149db48a59c7fdced29ff597c42
Kayla Beigh <kaylabeigh@Kaylas-MacBook-Pro-9.local>
2025-09-02 21:53:45 -0400

    hash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Dashboard from './pages/dash';
 import Report from './pages/summary';
